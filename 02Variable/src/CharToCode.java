44279fd32d3da974029a607df45eda896c9e8330
nareun130 <nareun130@gmail.com>
2023-07-06 01:05:25 +0900

    [FEAT] : char를 int값으로

---
+++ 02Variable/src/CharToCode.java
@@ -0,0 +1,17 @@
+
+public class CharToCode {
+	public static void main(String[] args) {
+		char ch = 'A';
+		int code = (int)ch;
+		
+		System.out.printf("%c=%d(%#X)%n",ch,code,code);
+		
+		char hch = '가';
+		System.out.printf("%c=%d(%#X)%n",hch,(int)hch,(int)hch);
+		
+		int hch2 = 0xAC00;
+		System.out.printf("%c",hch2);
+	}
+	
+
+}
