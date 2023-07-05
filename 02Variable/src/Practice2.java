6aa145d2ba2b08bee693c97d63fab8f08333d535
nareun130 <nareun130@gmail.com>
2023-07-06 02:15:27 +0900

    [FEAT] : 연습문제 출력

---
+++ 02Variable/src/Practice2.java
@@ -0,0 +1,17 @@
+
+public class Practice2 {
+	public static void main(String[] args) {
+		long regNo = 9801301069014L;
+
+		System.out.println("1" + "2");
+		System.out.println(true + "");// true
+		System.out.println('A' + 'B');// 131
+		System.out.println('1' + 2);// 51
+		System.out.println('1' + '2');// 99
+		System.out.println('J' + "ava");// Java
+//		System.out.println(true + null);//오류
+		double d = 1.4e3f;
+		System.out.println(d);
+		
+	}
+}
