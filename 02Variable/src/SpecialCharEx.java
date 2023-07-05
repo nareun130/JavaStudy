43b54a23ce1d5dca334d51bae79be0a21d340ce0
nareun130 <nareun130@gmail.com>
2023-07-06 01:19:17 +0900

    [FEAT] : 특수문자 출력

---
+++ 02Variable/src/SpecialCharEx.java
@@ -0,0 +1,12 @@
+
+public class SpecialCharEx {
+	public static void main(String[] args) {
+		System.out.println('\'');// ' 
+		System.out.println("'");// '
+		System.out.println("abc\t123\b456");// abc	12456
+		System.out.println('\n');
+		System.out.println("\"Hello\"");// "Hello"
+		System.out.println("c:\\");// c"\
+		System.out.println("\u0041");// A
+	}
+}
