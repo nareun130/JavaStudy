36d36a6ef862534a9029eb90385b788241957515
nareun130 <nareun130@gmail.com>
2023-07-06 16:56:15 +0900

    [FEAT] : 문자 숫자 비교

--- 03Operator/src/OperatorEx25.java
+++ 03Operator/src/OperatorEx25.java
@@ -12,6 +12,7 @@ public class OperatorEx25 {
 		if ('0' <= ch && ch <= '9') {
 			System.out.printf("입력한 문자는 숫자.%n");
 		}
+		
 		if(('a'<=ch && ch<='z')||('A'<=ch&&ch<='Z')) {
 			System.out.printf("입력한 문자는 영문자.%n");
 		}
