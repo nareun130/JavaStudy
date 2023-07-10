c60d0d025301ae5597d17f65c6695913a05d9a2c
nareun130 <nareun130@gmail.com>
2023-07-10 18:27:24 +0900

    [FEAT] : 참조 연산자로 for문 출력 비교

---
+++ 04Condition/src/FlowEx23.java
@@ -0,0 +1,17 @@
+
+public class FlowEx23 {
+	public static void main(String[] args) {
+		int i = 5;
+
+		while (i-- != 0) {
+			System.out.println(i);
+			System.out.println(i + " - I can do it.");
+		}
+		i = 5;
+		while (--i != 0) {
+			System.out.println(i);
+			System.out.println(i + " - I can do it.");
+		}
+
+	}
+}
