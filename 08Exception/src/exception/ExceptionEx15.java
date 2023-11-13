2fc9c34e8344116bbdd6b28f053982e9d8ae4356
김성호 <nareun130@gmail.com>
2023-11-13 23:51:00 +0900

    [FEAT] : finally 구문 이용

---
+++ 08Exception/src/exception/ExceptionEx15.java
@@ -0,0 +1,35 @@
+package exception;
+
+import java.io.File;
+
+public class ExceptionEx15 {
+	public static void main(String[] args) {
+		File f = createFile(args[0]);
+		System.out.println(f.getName() + " 파일이 성공적으로 생성됨.");
+
+	}
+
+	private static File createFile(String fileName) {
+		try {
+			if (fileName == null || fileName.equals(""))
+				throw new Exception("파일 이름 유효x");
+		} catch (Exception e) {
+			fileName = "untitled.txt";
+		} finally {
+			File f = new File(fileName);
+			createNewFile(f);
+
+			return f;
+
+		}
+
+	}
+
+	private static void createNewFile(File f) {
+		try {
+			f.createNewFile();
+		} catch (Exception e) {
+
+		}
+	}
+}
